2a2bbbbadf0bfdbc5d42d92b614794b326c4c3f1
Roberto Erick <robertoerickam@gmail.com>
2023-11-10 20:21:37 -0600

    maquetacion y parcialmente diseñada la apgina a partir de los boocetos otorgados

---
+++ script.js
@@ -0,0 +1,43 @@
+function salirMenu(){
+    window.location.href ="../index.html";
+}
+
+function citas(){
+    window.location.href = "./citas.html";
+}
+
+function volverMenu(){
+    window.location.href = "./menu_inicial.html";
+}
+
+function altaCitas(){
+    window.location.href = "./alta_cita.html";
+}
+
+function volverCitas(){
+    window.location.href = "./citas.html";
+}
+
+function clientes(){
+    window.location.href = "./clientes.html";
+}
+
+function altaClientes(){
+    window.location.href = "./alta_clientes.html";
+}
+
+function volverClientes(){
+    window.location.href = "./clientes.html";
+}
+
+function empleados(){
+    window.location.href = "./empleados.html";
+}
+
+function altaEmpleados(){
+    window.location.href = "./alta_empleados.html";
+}
+
+function volverEmpleados(){
+    window.location.href = "./empleados.html";
+}
